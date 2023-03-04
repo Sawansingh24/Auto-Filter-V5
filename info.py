1b8ca75cc399dc249a349ba85ff2da11646b500b
Sawansingh24 <119250143+Sawansingh24@users.noreply.github.com>
2023-03-04 22:18:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001818643949))
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001802248939))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/MOVIES_RCN')
-HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://youtu.be/zwHJLCrri6w')
+HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://youtu.be/LKqRcbmvlVQ')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001606185333))
